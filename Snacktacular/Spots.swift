659a20e13c06c8e04315d58819842dcd79ab731e
Christopher Greene <greenecr@bc.edu>
2020-04-13 10:55:21 -0400

    9.6 complete

--- Snacktacular/Spots.swift
+++ Snacktacular/Spots.swift
@@ -7,7 +7,35 @@
 //
 
 import Foundation
+import Firebase
 
 class Spots {
     var spotArray = [Spot]()
+    var db: Firestore!
+    
+    init() {
+        db = Firestore.firestore()
+    }
+    
+    func loadData(completed: @escaping () -> ()) {
+        db.collection("spots").addSnapshotListener { (querySnapshot, error) in
+            guard error == nil else {
+                print("Error: adding the snapshot listener")
+                return completed()
+            }
+            
+            self.spotArray = []
+            // there are querysnapshot!.documents.count in snapshot
+            for document in querySnapshot!.documents {
+                let spot = Spot(dictionary: document.data())
+                spot.documentID = document.documentID
+                self.spotArray.append(spot)
+            }
+            
+            completed()
+            
+        }
+    }
+    
+    
 }
