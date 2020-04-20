acdbf792a0340638812760099438007cf7c1489f
Christopher Greene <greenecr@bc.edu>
2020-04-20 12:49:02 -0400

    9.12 complete

---
+++ Snacktacular/ReviewTableViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ReviewTableViewController.swift
+//  Snacktacular
+//
+//  Created by Christopher Greene on 4/20/20.
+//  Copyright © 2020 Christopher Greene. All rights reserved.
+//
+
+import UIKit
+
+class ReviewTableViewController: UITableViewController {
+    
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var postedByLabel: UILabel!
+    @IBOutlet weak var reviewTitle: UITextField!
+    @IBOutlet weak var reviewDateLabel: UILabel!
+    @IBOutlet weak var reviewView: UITextView!
+    @IBOutlet weak var cancelBarButton: UIBarButtonItem!
+    @IBOutlet weak var saveBarButton: UIBarButtonItem!
+    @IBOutlet weak var deleteButton: UIButton!
+    @IBOutlet weak var buttonsBackgroundView: UIView!
+
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:)))
+        tap.cancelsTouchesInView = false
+        self.view.addGestureRecognizer(tap)
+    }
+    
+    func leaveViewController() {
+        let isPresentingInAddMode = presentingViewController is UINavigationController
+        if isPresentingInAddMode {
+            dismiss(animated: true, completion: nil)
+        } else {
+            navigationController?.popViewController(animated: true)
+        }
+    }
+    
+    @IBAction func reviewTitleChanged(_ sender: UITextField) {
+    }
+    
+    @IBAction func returnTitleDonePressed(_ sender: UITextField) {
+    }
+    
+    @IBAction func deleteButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        leaveViewController()
+    }
+    
+    @IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
+    }
+}
